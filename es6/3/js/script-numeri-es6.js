5be48e7ad8843bbdeebf67ac55d2125e7baf4b29
Teresa Spigoni <teresaspigoni@gmail.com>
2021-01-20 14:27:49 +0100

    js-snak-vanilla 3 e js-snak-es6 3

--- es6/3/js/script-numeri-es6.js
+++ es6/3/js/script-numeri-es6.js
@@ -1,8 +1,5 @@
 // Si scriva una funzione che accetti tre argomenti, un array e due numeri (A più piccolo di B).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
-
-const mainArray = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
 const returnArray = (array, num1, num2) => {
   let newArray = [];
   array.forEach((item) => {
@@ -12,5 +9,7 @@ const returnArray = (array, num1, num2) => {
   });
   return newArray;
 }
+
+const mainArray = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 console.log(returnArray(mainArray, 10, 20));
--- vanilla/3/js/script-numeri-vanilla.js
+++ vanilla/3/js/script-numeri-vanilla.js
@@ -1,8 +1,5 @@
 // Si scriva una funzione che accetti tre argomenti, un array e due numeri (A più piccolo di B).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
-
-var mainArray = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
 function returnArray(array, num1, num2) {
   var newArray = [];
   for (var i = 0; i < array.length; i++) {
@@ -12,5 +9,7 @@ function returnArray(array, num1, num2) {
   }
   return newArray;
 }
+
+var mainArray = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 console.log(returnArray(mainArray, 10, 20));
