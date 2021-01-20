74675e903b10cc52300677caac7dfc703bbf954e
Teresa Spigoni <teresaspigoni@gmail.com>
2021-01-20 14:21:11 +0100

    js-snak-es6 3

--- es6/3/js/script-numeri-es6.js
+++ es6/3/js/script-numeri-es6.js
@@ -1,2 +1,16 @@
 // Si scriva una funzione che accetti tre argomenti, un array e due numeri (A più piccolo di B).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
+
+const mainArray = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+const returnArray = (array, num1, num2) => {
+  let newArray = [];
+  array.forEach((item) => {
+    if (array.indexOf(item) > array.indexOf(num1) && array.indexOf(item) < array.indexOf(num2)) {
+      newArray.push(item);
+    }
+  });
+  return newArray;
+}
+
+console.log(returnArray(mainArray, 10, 20));
